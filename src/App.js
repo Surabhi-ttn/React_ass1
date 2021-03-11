3c26db8987a149b140fde71f10d49bd66d9655ae
surabhi-ttn <surabhi.chaurasia@tothenew.com>
2021-03-12 00:32:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  counter= 0;
+  state = {
+    item: "",
+    fruits: [],
+  }
+
+  handleOnChange = (e) => {
+    let str = e.target.value
+    this.setState(
+      {
+        ...this.state,
+        item :str
+      }
+    )
+  }
+
+  handleOnClick = () => {
+    let fruit= this.state.item.split('-')[0]
+    let quantity= this.state.item.split('-')[1]
+    this.setState(
+      {
+        ...this.state,
+        fruits: [...this.state.fruits,{'fruit':fruit, 'qty':quantity, 'id':this.counter}]
+      }
+    )
+    this.counter++;
+  }
+
+  handleOnDelete = (e) => {
+    //console.log("hello", e.target.id,typeof e.target.id)
+    let id= parseInt(e.target.id)
+    //console.log(id ,typeof id)
+    let buffer =  this.state.fruits
+    buffer = buffer.filter(fruit => fruit.id!=id)
+    console.log(buffer)
+    this.setState(
+      {
+        ...this.state,
+        fruits : buffer
+      }
+    )
+    
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={this.handleOnChange}></input>
+      <button onClick={this.handleOnClick}>Submit</button>
+      <ul>
+        {
+          this.state.fruits.map(fruit =>
+              <li>
+                <span>{fruit.fruit}</span>
+                <span>{fruit.qty}</span>
+                <button id={fruit.id} onClick={this.handleOnDelete}>Delete</button>
+              </li>
+            )
+        }
+      </ul>
     </div>
   );
 }
-
+}
 export default App;
